fccfd322947be46d4c987871b345d3da54f6eb44
hasnat-sumit <khsumit28@gmail.com>
2026-09-13 19:30:52 +0600

    2nd commit

--- src/Components/ExploreTechnologies.tsx
+++ src/Components/ExploreTechnologies.tsx
@@ -171,7 +171,7 @@ const ExploreTechnologies = ({ technologies }: { technologies: Technology[] }) =
 
     return (
         <div className='flex flex-col lg:flex-row justify-between gap-4 px-4 lg:px-0'>
-            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 w-full">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
                 {technologies.map((technology: Technology) => {
                     const selected = isSelected(technology);
                     return (
