4028be5ee3b4f1d8fcd29cda712b75e5cca9873e
hasnat-sumit <khsumit28@gmail.com>
2026-09-13 19:50:43 +0600

    3rd commit

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,31 +1,3 @@
-// import bannerLogo from "../assets/banner-stack.png"
-
-// const Banner = () => {
-//     return (
-//         <section className="flex my-6 container mx-auto items-center gap-10 mt-20 mb-20">
-//             <div className='mb-6'>
-//                 <h2 className='font-bold text-6xl'>Build Your Ideal</h2>
-//                 <h2 className='font-bold text-6xl mb-4 bg-linear-to-r from-orange-500 via-pink-700 to-violet-600 bg-clip-text text-transparent'>Development Stack</h2>
-//                 <p className="text-xl text-gray-500">Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your
-//                     next project.</p>
-//                 <div className="flex gap-6 my-6 mt-15">
-//                     <button className="btn bg-linear-to-r from-orange-500 to-pink-500 text-white px-6 py-2 rounded-lg">Explore Technologies</button>
-//                     <button className="btn rounded-lg text-gray-500">Learn More</button>
-//                 </div>
-
-//             </div>
-
-//             <div>
-//                 <img className = 'w-4xl' src={bannerLogo} alt="" />
-//             </div>
-//         </section>
-
-//     );
-// };
-
-
-// export default Banner;
-
 import bannerLogo from "../assets/banner-stack.png"
 
 const Banner = () => {
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,33 +1,3 @@
-// import Logo from '../assets/logo-text.png'
-
-// const Nav = () => {
-//     return (
-//         <nav className="my-6 sticky top-0 z-100 py-4 bg-white" >
-//             <div className="flex justify-between container mx-auto">
-//                 <img className='w-40' src={Logo} alt="" />
-
-//                 <ul className='flex gap-6 items-center text-bolder text-gray-500 '>
-//                     <li className='text-pink-700'>Home</li>
-//                     <li>Technologies</li>
-//                     <li>Projects</li>
-//                     <li>About</li>
-//                     <li>Contact</li>
-//                 </ul>
-
-//                 <div className='flex gap-4'>
-//                     <button className=''>Sign In</button>
-//                     <button className='btn bg-pink-500 text-white rounded-full'>Sign Up</button>
-//                 </div>
-
-//             </div>
-
-//         </nav>
-//     );
-// };
-
-// export default Nav;
-
-
 import { useState } from 'react';
 import Logo from '../assets/logo-text.png'
 import { FaBars, FaTimes } from 'react-icons/fa';
